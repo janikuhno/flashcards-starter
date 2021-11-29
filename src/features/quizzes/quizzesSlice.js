e18cd5f06c5754b2518edec597e07a6325741056
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-11-29 13:10:28 +0200

    Created quizzesSlice

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addQuizIdForTopic } from "../topics/topicsSlice";
+
+export const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: {
+        quizzes: {}
+    },
+    reducers: {
+        addQuiz: (state, action) => {
+            const { id } = action.payload;
+            state.quizzes[id] = action.payload;
+        }
+    }
+});
+
+// a thunk for dispatching two actions one after the other
+export const addQuizForTopicId = (quiz) => {
+    const { topicId, id } = quiz;
+    return (dispatch) => {
+        dispatch(quizzesSlice.actions.addQuiz(quiz));
+        dispatch(addQuizIdForTopic({ topicId: topicId, id: id }));
+    };
+};
+
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
